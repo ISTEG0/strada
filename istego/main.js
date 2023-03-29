45f44061d06f1f344127a2a505a6c854f3c39cb0
Ivan Marahovskii <marahovskii81@gmail.com>
2023-03-29 21:38:53 +0500

    practice/object-delete-add

--- istego/main.js
+++ istego/main.js
@@ -1,48 +1,14 @@
-const phoneBook = {
-    "Артем Димитров": 89823673498,
-    "Ярослав Менторович": 89867678999,
-    "Михаил Королев": 89833355523,
-    "Strada": 89128424274,
-    "Настя": 89123467865,
-    "Илья": 89127687233
-}
-
-const whoseNumber = 'Чей номер вас интересует? Только вводите как в консоли, можете просто скопировать)';
-const errorEntered = 'Кажется вы ввели не правильно, попробуйте снова, без пробелов, без ничего, как в консоли!';
-const cancelProgram = 'Вы отменили работу программы!';
-const questionStart = confirm('Хотите посмотреть мою телефонную книгу?');
-
-if (questionStart === true) {
-    const notice = confirm('Здорово, ОТКРОЙТЕ КОНСОЛЬ разработчика и нажмите ОК, список имен появится там!')
-    if (notice === true) {
-        for (phone in phoneBook) {
-            console.log(phone);
-        }
-        let question = prompt(whoseNumber);
-        if (question === null) {
-            alert(cancelProgram);
-        } else if (phoneBook[question] === undefined) {
-            while (phoneBook[question] === undefined) {
-                alert(errorEntered);
-                question = prompt(whoseNumber);
-                if (question === null) {
-                    alert(cancelProgram);
-                    break;
-                } else if (phoneBook[question] === undefined) {
-                    alert(errorEntered);
-                    question = prompt(whoseNumber);
-                } else {
-                    alert('Ну вот вам и номер ' + question + ': ' + phoneBook[question]);
-                }
-            }
-        } else {
-            alert('Ну вот вам и номер ' + question + ': ' + phoneBook[question]);
-        }
-    }
-    else {
-        alert(cancelProgram);
+const obj = {
+    a: 1234,
+    b: 2345,
+    c: 234234,
+    add (name, number) {
+        this[name] = number;
     }
 }
-else {
-    alert(cancelProgram);
-}
+
+obj.test = 333;
+
+delete obj.a;
+
+console.log(obj);
