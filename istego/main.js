6f78879081f5a91a214e11b346bcf6710bdd6f80
Ivan Marahovskii <marahovskii81@gmail.com>
2023-03-29 22:38:16 +0500

    add functional addition contacts and delete contacts

--- istego/main.js
+++ istego/main.js
@@ -7,12 +7,15 @@ const phoneBook = {
     "Илья": 89127687233
 }
 
+const questionStart = +prompt('Если хотите просмотреть список контактов введите 1, добавить контакт введите 2, удалить контакт 3');
 const whoseNumber = 'Чей номер вас интересует? Только вводите как в консоли, можете просто скопировать)';
 const errorEntered = 'Кажется вы ввели не правильно, попробуйте снова, без пробелов, без ничего, как в консоли!';
 const cancelProgram = 'Вы отменили работу программы!';
-const questionStart = confirm('Хотите посмотреть мою телефонную книгу?');
 
-if (questionStart === true) {
+let phoneName = null;
+let phoneNumber = null;
+
+if (questionStart === 1) {
     const notice = confirm('Здорово, ОТКРОЙТЕ КОНСОЛЬ разработчика и нажмите ОК, список имен появится там!')
     if (notice === true) {
         for (phone in phoneBook) {
@@ -42,7 +45,30 @@ if (questionStart === true) {
     else {
         alert(cancelProgram);
     }
+} else if (questionStart === 2) {
+    phoneName = prompt('Введите имя');
+    phoneNumber = +prompt('Введите номер');
+    addContact(phoneName, phoneNumber);
+} else if(questionStart === 3) {
+    for (phone in phoneBook) {
+        console.log(phone);
+    }
+    phoneName = prompt('Введите имя контакта, который хотите удалить');
+    deleteContact(phoneName);
 }
 else {
     alert(cancelProgram);
+}
+
+
+function addContact(Name, Number) {
+    phoneBook[Name] = Number;
+    console.log('Запись добавлена!');
+    console.log(phoneBook);
+}
+
+function deleteContact(nameDelete) {
+    delete phoneBook[nameDelete];
+    console.log('Запись удалена!');
+    console.log(phoneBook);
 }
