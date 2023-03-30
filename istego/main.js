de2f50842b34d2dc8a2ebafd40cd817281dadaf0
Ivan Marahovskii <marahovskii81@gmail.com>
2023-03-30 17:48:34 +0500

    practice/object/copy

--- istego/main.js
+++ istego/main.js
@@ -2,13 +2,18 @@ const obj = {
     a: 1234,
     b: 2345,
     c: 234234,
-    add (name, number) {
-        this[name] = number;
+    test: {
+        g: 7777,
+        j: 88888
     }
 }
 
-obj.test = 333;
 
-delete obj.a;
+const clone = {};
+
+Object.assign(clone,obj);
+
+clone.test.j = 9999;
 
 console.log(obj);
+console.log(clone);
